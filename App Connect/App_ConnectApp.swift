e1c653c1a3e1c1930c2393a8655f65837a29b204
Henry Paulino <henrypl360@gmail.com>
2020-07-08 22:58:13 -0400

    Initial Commit

---
+++ App Connect/App_ConnectApp.swift
@@ -0,0 +1,17 @@
+//
+//  App_ConnectApp.swift
+//  App Connect
+//
+//  Created by Henry Paulino on 7/8/20.
+//
+
+import SwiftUI
+
+@main
+struct App_ConnectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
